36c9f168a3be6023f6dc91924adbb74dd160bc80
Nicholas Hathaway <nickjhathaway@gmail.com>
2014-07-24 11:35:36 -0400

    added color coding to the bookmarking

--- getLocation.sh
+++ getLocation.sh
@@ -2,14 +2,13 @@
 
 #First check to see if there is only one argument
 if [[ $# -ne 1 ]]; then
-	echo "Takes only one argument, name for bookmark for current location"
-	echo "eg. getLocation.sh bookmark"
+	echo "\033[1;38;5;196mTakes only one argument, name for bookmark for current location"
+	echo "eg. getLocation.sh bookmark\033[0m"
 	exit
 fi
-
 check=$(egrep "^$1\>" $LOCBOOKHOME/.ohThePlacesIveBeen.txt | cut -d$'\t' -f2)
 if [ -n "$check" ]; then
     echo $check
 else
-	echo "No bookmarked named $1"
+	echo "\033[1;38;5;69mNo bookmarked named \033[1;38;5;196m$1"
 fi
--- removeLocation.sh
+++ removeLocation.sh
@@ -2,8 +2,8 @@
 
 #First check to see if there is only one argument
 if [[ $# -ne 1 ]]; then
-	echo "Takes only one argument, name for bookmark for the location to delete"
-	echo "eg. removeLocation.sh bookmarkName"
+	echo "\033[1;38;5;196mTakes only one argument, name for bookmark for the location to delete"
+	echo "eg. removeLocation.sh bookmarkName\033[0m"
 	exit
 fi
 
@@ -16,5 +16,5 @@ if [ -n "$check" ]; then
 	 	sed -i "/^$1\b/d" $LOCBOOKHOME/.ohThePlacesIveBeen.txt
 	fi
 else
-	echo -e "No bookmarked location save as $1"
+	echo -e "\033[1;38;5;69mNo bookmarked location saved as \033[1;38;5;196m$1\033[0m"
 fi
