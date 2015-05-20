7fe210da7a53b791a49a8d956faae4baba37f953
Nicholas Hathaway <nickjhathaway@gmail.com>
2015-05-20 19:29:52 -0400

    need -e with echo for color

--- getLocation.sh
+++ getLocation.sh
@@ -3,8 +3,8 @@
 
 #First check to see if there is only one argument
 if [[ $# -ne 1 ]]; then
-	echo "\033[1;38;5;196mTakes only one argument, name for bookmark for current location"
-	echo "eg. getLocation.sh bookmark\033[0m"
+	echo -e "\033[1;38;5;196mTakes only one argument, name for bookmark for current location"
+	echo -e "eg. getLocation.sh bookmark\033[0m"
 	exit
 fi
 
@@ -12,5 +12,5 @@ check=$(egrep "^$1\>" $LOCBOOKHOME/.ohThePlacesIveBeen.txt | cut -d$'\t' -f2)
 if [ -n "$check" ]; then
     echo $check
 else
-	echo "\033[1;38;5;69mNo bookmarked named \033[1;38;5;196m$1"
+	echo -e "\033[1;38;5;69mNo bookmarked named \033[1;38;5;196m$1"
 fi
